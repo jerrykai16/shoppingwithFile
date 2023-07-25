d3a3453aabec2f059dfb9c3541cd120dbf83b51f
Jerrykai <k86887961@gmail.com>
2023-07-25 13:51:05 +0800

    phpmailer setting

--- buy.php
+++ buy.php
@@ -35,14 +35,14 @@
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'kaikuan759@gmail.com';                     //SMTP username
-        $mail->Password   = 'tfpdudvcomovbhnl';                               //SMTP password
+        $mail->Username   = 'yourEmail@gmail.com';                     //SMTP username
+        $mail->Password   = 'yourPassword';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
         //Recipients
-        $mail->setFrom('kaikuan759@gmail.com');
-        $mail->addAddress('kaikuan759@gmail.com');     //Add a recipient
+        $mail->setFrom('yourEmail@gmail.com');
+        $mail->addAddress('recipient@gmail.com');     //Add a recipient
 
 
         //Content
